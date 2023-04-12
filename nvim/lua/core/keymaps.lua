1dbea5e26f5bd6847c54523997604e18976e4146
Dan <57308957+D4NZ-jpg@users.noreply.github.com>
2023-04-11 21:03:05 -0600

    Improved c++ and cmake integration. Added more Snippets

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -7,57 +7,59 @@ vim.keymap.set("n", "<Right>", "<nop>")
 vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv") -- Move selection up
 vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv") -- Move selection down
 
-vim.keymap.set("n", "J", "mzJ`z") -- Bring next line to the end of the current one without moving the cursor
+vim.keymap.set("n", "J", "mzJ`z")            -- Bring next line to the end of the current one without moving the cursor
 
-vim.keymap.set("n", "Q", "<nop>") -- Disable Q
+vim.keymap.set("n", "Q", "<nop>")            -- Disable Q
 
 -- Move to errors
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<cr>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<cr>zz")
 
+-- Move between snippet parameters
+vim.keymap.set("i", "<C-k>", "<cmd>lua require('luasnip').jump(1)<cr>")
+vim.keymap.set("i", "<C-j>", "<cmd>lua require('luasnip').jump(-1)<cr>")
+
 -- Add git diff as command
 vim.cmd([[command! Diff :DiffviewOpen]])
 
 return {
-	["<leader>f"] = {
-		name = "+File/Find",
-		f = { "<cmd>Telescope find_files<cr>", "Find File" },
-		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-		n = { "<cmd>enew<cr>", "New File" },
-		t = { "<cmd>Telescope file_browser<cr>", "Browse Files" },
-		h = { "<cmd>Telescope help_tags<cr>", "Help" },
-		l = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-	},
+    ["<leader>f"] = {
+        name = "+File/Find",
+        f = { "<cmd>Telescope find_files<cr>", "Find File" },
+        r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+        n = { "<cmd>enew<cr>", "New File" },
+        t = { "<cmd>Telescope file_browser<cr>", "Browse Files" },
+        h = { "<cmd>Telescope help_tags<cr>", "Help" },
+        l = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+    },
+    ["<C-t>"] = {
+        name = "+Tabs",
+        a = { "<cmd>$tabnew<cr><cmd>Alpha<cr>", "New Tab" },
+        c = { "<cmd>tabclose<cr>", "Close Tab" },
+        o = { "<cmd>tabonly<cr>", "Close Others" },
+        h = { "<cmd>tabp<cr>", "Previous Tab" },
+        l = { "<cmd>tabn<cr>", "Next Tab" },
+        g = { "<cmd>execute 'tabn ' .. input('Tab number: ')<cr>", "Jump to tab number" },
 
-	["<C-t>"] = {
-		name = "+Tabs",
-		a = { "<cmd>$tabnew<cr><cmd>Alpha<cr>", "New Tab" },
-		c = { "<cmd>tabclose<cr>", "Close Tab" },
-		o = { "<cmd>tabonly<cr>", "Close Others" },
-		h = { "<cmd>tabp<cr>", "Previous Tab" },
-		l = { "<cmd>tabn<cr>", "Next Tab" },
-		g = { "<cmd>execute 'tabn ' .. input('Tab number: ')<cr>", "Jump to tab number" },
-
-		m = {
-			name = "+Move",
-			h = { "<cmd>-tabmove<cr>", "Move current tab to previous position" },
-			l = { "<cmd>+tabmove<cr>", "Move current tab to next position" },
-		},
-	},
-
-	["<leader>x"] = {
-		name = "+Trouble",
-		x = { "<cmd>TroubleToggle<cr>", "Toggle Trouble Panel" },
-		w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Toggle workspace diagnostics" },
-		d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Toggle document diagnostics" },
-		q = { "<cmd>TroubleToggle loclist<cr>", "Toggle location list" },
-		l = { "<cmd>TroubleToggle quickfix<cr>", "Toggle quickfix" },
-		r = { "<cmd>TroubleToggle lsp_references<cr>", "Toggle references" },
-	},
-
-	["<leader>p"] = { '"_dp', "Void paste", mode = "x" },
-	["<leader>k"] = { "<cmd>lnext<cr>zz", "Lnext" },
-	["<leader>j"] = { "<cmd>lprev<cr>zz", "Lprev" },
-	["<leader>r"] = { ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", "Replace current word" },
-	["<leader>t"] = { "<cmd>lua require('betterTerm').open()<cr>", "Toggle terminal" },
+        m = {
+            name = "+Move",
+            h = { "<cmd>-tabmove<cr>", "Move current tab to previous position" },
+            l = { "<cmd>+tabmove<cr>", "Move current tab to next position" },
+        },
+    },
+    ["<leader>x"] = {
+        name = "+Trouble",
+        x = { "<cmd>TroubleToggle<cr>", "Toggle Trouble Panel" },
+        w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Toggle workspace diagnostics" },
+        d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Toggle document diagnostics" },
+        q = { "<cmd>TroubleToggle loclist<cr>", "Toggle location list" },
+        l = { "<cmd>TroubleToggle quickfix<cr>", "Toggle quickfix" },
+        r = { "<cmd>TroubleToggle lsp_references<cr>", "Toggle references" },
+    },
+    ["<leader>p"] = { '"_dp', "Void paste", mode = "x" },
+    ["<leader>k"] = { "<cmd>lnext<cr>zz", "Lnext" },
+    ["<leader>j"] = { "<cmd>lprev<cr>zz", "Lprev" },
+    ["<leader>r"] = { ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", "Replace current word" },
+    ["<leader>t"] = { "<cmd>lua require('betterTerm').open()<cr>", "Toggle terminal" },
+    ["<leader>c"] = "+CMake"
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,67 +1,91 @@
 return {
 
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v1.x",
-		event = { "BufReadPost", "BufNewFile" },
-		dependencies = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/mason.nvim", config = true },
-			{ "williamboman/mason-lspconfig.nvim" },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v1.x",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim",          config = true },
+            { "williamboman/mason-lspconfig.nvim" },
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lua" },
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lua" },
 
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-		config = function()
-			local lsp = require("lsp-zero").preset({
-				name = "recommended",
-				set_lsp_keymaps = { preserve_mappings = false },
-			})
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+        },
+        config = function()
+            local lsp = require("lsp-zero").preset({
+                name = "recommended",
+                set_lsp_keymaps = { preserve_mappings = false },
+            })
 
-			lsp.ensure_installed({
-				"eslint",
-				"lua_ls",
-				"rust_analyzer",
-			})
+            lsp.ensure_installed({
+                "eslint",
+                "lua_ls",
+                "rust_analyzer",
+            })
 
-			lsp.nvim_workspace()
-			lsp.setup()
-		end,
-	},
+            lsp.nvim_workspace()
+            lsp.setup()
 
-	-- null-ls
-	{
-		"jay-babu/mason-null-ls.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"jose-elias-alvarez/null-ls.nvim",
-		},
-		event = { "BufReadPost", "BufNewFile" },
-		opts = {
-			ensure_installed = { "stylelua", "eslint" },
-			automatic_setup = true,
-		},
+            require("luasnip.loaders.from_snipmate").lazy_load()
+        end,
+    },
 
-		config = function(_, opts)
-			require("mason-null-ls").setup(opts)
+    -- null-ls
+    {
+        "jay-babu/mason-null-ls.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "jose-elias-alvarez/null-ls.nvim",
+        },
+        event = { "BufReadPost", "BufNewFile" },
+        opts = {
+            ensure_installed = { "stylelua", "eslint" },
+            automatic_setup = true,
+        },
+        config = function(_, opts)
+            require("mason-null-ls").setup(opts)
 
-			handlers = {
-				function(source_name, methods)
-					require("mason-null-ls.automatic_setup")(source_name, methods)
-				end,
-			},
+            handlers = {
+                    function(source_name, methods)
+                        require("mason-null-ls.automatic_setup")(source_name, methods)
+                    end,
+                },
 
-			require("null-ls").setup()
-		end,
-	},
+                require("null-ls").setup()
+        end,
+    },
+
+    -- Configure cmake for c++
+    {
+        'cdelledonne/vim-cmake',
+        keys = {
+            {
+                "<leader>cg",
+                "<cmd>CMakeGenerate build<cr>",
+                desc =
+                "Generate build system"
+            },
+            {
+                "<leader>cb",
+                "<cmd>CMakeBuild <cr>",
+                desc =
+                "Build project"
+            },
+        },
+        config = function()
+            vim.g.cmake_link_compile_commands = 1
+            vim.g.cmake_generate_options = { "-G MinGW Makefiles" }
+        end
+    }
 }
