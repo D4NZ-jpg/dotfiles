d4eb9adc33116d04af413627743384c38820a732
Dan <57308957+D4NZ-jpg@users.noreply.github.com>
2023-04-21 01:17:23 -0600

    added games cuz why not

---
+++ nvim/lua/plugins/games.lua
@@ -0,0 +1,13 @@
+return {
+    -- minesweeper
+    {
+        "seandewar/nvimesweeper",
+        cmd = "Nvimesweeper"
+    },
+
+    -- Blacjack
+    {
+        "alanfortlink/blackjack.nvim",
+        cmd = "BlackJackNewGame"
+    }
+}
