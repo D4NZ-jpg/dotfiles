798664752525e6d94f0001b850239daa6ceef5ad
Dan <57308957+D4NZ-jpg@users.noreply.github.com>
2023-04-28 09:35:04 -0600

    More games

--- nvim/lua/plugins/games.lua
+++ nvim/lua/plugins/games.lua
@@ -9,5 +9,17 @@ return {
     {
         "alanfortlink/blackjack.nvim",
         cmd = "BlackJackNewGame"
+    },
+
+    -- Tetris
+    {
+        "alec-gibson/nvim-tetris",
+        cmd = "Tetris"
+    },
+
+    -- Cellular-automaton
+    {
+        "Eandrju/cellular-automaton.nvim",
+        cmd = "CellularAutomaton"
     }
 }
