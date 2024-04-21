6b043d5db57286f69a11b537803fd2b3c0374f7e
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2024-04-21 09:13:13 -0600

    firefox: Added user overrides

--- setup/firefox/user-overrides.js
+++ setup/firefox/user-overrides.js
@@ -1,6 +1,15 @@
+// Make it more convenient to use (in exchange of accepting more fingerprinting)
 user_pref("privacy.resistFingerprinting.letterboxing", false);
 user_pref("privacy.resistFingerprinting", false);
 user_pref("privacy.resistFingerprinting.pbmode", false);
 user_pref("webgl.disabled", false);
 
+// Enable userChrome.css
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Enable DRM streaming
+user_pref("media.eme.enabled", true)
+
+user_pref("signon.rememberSignons", false);       // Disable ask to save password
+user_pref("browser.tabs.loadInBackground", true); // Switch to new tabs
+user_pref("general.autoScroll", true);            // Allow auto-scroll with middle button
