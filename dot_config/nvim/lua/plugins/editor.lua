601a8ae74d7e75e94e159975aba0e7b0c41ca057
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2024-06-23 23:00:43 -0600

    nvim: hardtime
    Make sure to keep best vim motion practices

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -199,4 +199,13 @@ return {
             vim.notify = require("notify")
         end
     },
+
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        event = "VeryLazy",
+        opts = {
+            max_count = 5,
+        }
+    },
 }
