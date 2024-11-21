7a80ecc3db1e05950a27bf3ff76d1b672c1a2584
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2024-11-21 02:47:44 -0600

    feat(nvim): Flutter lsp support

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -339,4 +339,16 @@ return {
     --
     -- }
     --
+    -- Flutter
+    {
+        'nvim-flutter/flutter-tools.nvim',
+        lazy = false,
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'stevearc/dressing.nvim', -- optional for vim.ui.select
+        },
+        config = true,
+        ft = "dart"
+    }
+
 }
