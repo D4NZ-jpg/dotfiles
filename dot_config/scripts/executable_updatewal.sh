650e7963b9bd0d259a8abe36b019f8a976bfd245
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2024-12-06 23:43:38 -0600

    feat(matugen): Basic matugen setup for wallpaper based themes

--- dot_config/scripts/executable_updatewal.sh
+++ dot_config/scripts/executable_updatewal.sh
@@ -1,35 +1,6 @@
 #!/bin/sh
 set -e
 
-# Select random wallpaper
-wal --cols16 -q -i ~/wallpapers -n -s
-
-# Load pywal scheme
-source "$HOME/.cache/wal/colors.sh"
-cp "$HOME/.cache/wal/colors.Xresources" "$HOME/.Xresources"
-
-# set wallpaper
-swww img $wallpaper --transition-step 20 --transition-fps 20
-
-# copy current file to ~/currentWallpaper
-cp $wallpaper ~/.cache/currentWallpaper.jpg
-
-# Update kitty
-kill -USR1 $(pgrep kitty)
-
-# Copy discord css to correct folder 
-mkdir -p ~/.config/vesktop/themes
-cp ~/.cache/wal/discord-wal-theme.css ~/.config/vesktop/themes/pywal.theme.css
-
-
-# Reload waybar
-killall -SIGUSR2 waybar
-
-# Send update to firefox
-pywalfox update
-
-# Update spotify if spicetify installed (it will close it)
-if command -v spicetify &> /dev/null; then
-    spicetify apply
-fi
-
+#Randomize wallpaper
+cp "$(find $HOME/wallpapers -type f | shuf -n 1)" "$HOME/.cache/currentWallpaper.jpg"
+matugen image $HOME/.cache/currentWallpaper.jpg
