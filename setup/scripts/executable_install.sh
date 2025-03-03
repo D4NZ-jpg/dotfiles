1426db03c7d75119c69dc611fedf256962a32375
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2025-03-02 18:53:43 -0600

    feat(install script): install chaotic-aur

--- setup/scripts/executable_install.sh
+++ setup/scripts/executable_install.sh
@@ -4,10 +4,25 @@
 echo "dotfiles by Danz. (I use Arch btw)"
 set -e 
 
-source $HOME/setup/scripts/utils.sh
+source "$HOME"/setup/scripts/utils.sh
+
+# Install chaotic-aur
+# Check if chaotic-aur is already installed
+if ! grep -q "\[chaotic-aur\]" /etc/pacman.conf; then
+    echo "Chaotic-aur not found, installing..."
+    sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+    sudo pacman-key --lsign-key 3056513887B78AEB
+
+    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+    echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+else
+    echo "Chaotic-aur found, skipping..."
+fi
 
 # Install
-cp $HOME/setup/pkgs/pkgs.lst install.lst
+cp "$HOME"/setup/pkgs/pkgs.lst install.lst
 
 # Nvidia drivers (https://github.com/prasanthrangan/hyprdots/blob/main/Scripts/install.sh)
 if hasNvidia; then
@@ -16,7 +31,7 @@ if hasNvidia; then
     done
     IFS=$' ' read -r -d '' -a nvga < <(lspci -k | grep -E "(VGA|3D)" | grep -i nvidia | awk -F ':' '{print $NF}' | tr -d '[]()' && printf '\0')
     for nvcode in "${nvga[@]}"; do
-        awk -F '|' -v nvc="${nvcode}" '{if ($3 == nvc) {split(FILENAME,driver,"/"); print driver[length(driver)],"\nnvidia-utils"}}' $HOME/setup/.nvidia/nvidia*dkms >>install.lst
+        awk -F '|' -v nvc="$nvcode" '{if ($3 == nvc) {split(FILENAME,driver,"/"); print driver[length(driver)],"\nnvidia-utils"}}' "$HOME"/setup/.nvidia/nvidia*dkms >>install.lst
     done
     echo -e "\033[0;32m[GPU]\033[0m: detected // ${nvga[@]}"
 
@@ -40,22 +55,22 @@ while read LINE; do
 
     read -p "Would you like to install $name? [y/N]: " answer < /dev/tty
     if [[ $answer = [Yy] ]]; then
-        for pkg in $pkgs; do
+        for pkg in "${pkgs[@]}"; do
             echo "$pkg" >> install.lst 
         done
     fi
-done < $HOME/setup/pkgs/extras.lst
+done < "$HOME"/setup/pkgs/extras.lst
 
 # Install pkgs
 installPkgs install.lst
 
 # Configuring packages
-source $HOME/setup/scripts/post-install.sh
+source "$HOME"/setup/scripts/post-install.sh
 
 # Systemd
 while read service ; do
-    startCtl $service
-done < $HOME/setup/pkgs/system_ctl.lst
+    startCtl "$service"
+done < "$HOME"/setup/pkgs/system_ctl.lst
 
 echo ""
 echo "===================="
