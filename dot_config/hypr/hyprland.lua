e140c163acfa9e7f238eaa597ee10c270d621b0a
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2026-09-16 17:29:02 -0600

    Speed up workspace slide animations

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -33,7 +33,7 @@ hl.curve("myBezier", { type = "bezier", points = { { 0.05, 0.9 }, { 0.1, 1.05 } 
 for _, anim in ipairs({
     { "windows", 5, "myBezier" }, { "windowsOut", 5, "default", "popin 80%" },
     { "border", 7, "default" }, { "borderangle", 8, "default" },
-    { "fade", 5, "default" }, { "workspaces", 4, "default", "slidevert" },
+    { "fade", 5, "default" }, { "workspaces", 2.5, "default", "slidevert" },
 }) do
     hl.animation({ leaf = anim[1], enabled = true, speed = anim[2], bezier = anim[3], style = anim[4] })
 end
