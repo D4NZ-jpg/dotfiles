19c45a7a4109a95e18c1613d4919def775df832e
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2026-09-16 15:03:36 -0600

    Fix Zsh transient prompt and capture shell setup

--- dot_config/zsh/prompt.zsh
+++ dot_config/zsh/prompt.zsh
@@ -3,54 +3,32 @@
 bindkey -v # Vim mode
 bindkey -M viins 'kk' vi-cmd-mode # Escape with 'kk'
 
-## Transient prompt
-
-starship_transient_prompt_func() {
-  starship module character
-}
-
-starship_transient_rprompt_func() {
-  starship module time
-}
-
-### Enable transient prompt
-zle-line-init() {
-  emulate -L zsh
-  [[ $CONTEXT == start ]] || return 0
-
-  while true; do
-    zle .recursive-edit
-
-    local -i ret=$?
-
-    [[ $ret == 0 && $KEYS == $'\4' ]] || break
-    [[ -o ignore_eof ]] || exit 0
-
-  done
-
-  local saved_prompt=$PROMPT
-  local saved_rprompt=$RPROMPT
-
-  PROMPT=$'\n$(starship_transient_prompt_func) '
-  RPROMPT='$(starship_transient_rprompt_func)'
-
-  zle .reset-prompt
-
-  PROMPT=$saved_prompt
-  RPROMPT=$saved_rprompt
-
-  if ((ret)); then
-    zle .send-break
-  else
-    zle .accept-line
-  fi
-
-
-  return ret
-}
-
-zle -N zle-line-init
-
-# Evaluate
-
 eval "$(starship init zsh)"
+
+# Collapse only a finished command line. Do not recursively edit or accept input.
+# Native Zsh color escapes have correct display widths (unlike raw ANSI output).
+autoload -Uz add-zsh-hook add-zle-hook-widget
+
+_dan_transient_line_finish() {
+  [[ $CONTEXT == start ]] || return 0
+  typeset -g _dan_full_prompt=$PROMPT
+  typeset -g _dan_full_rprompt=$RPROMPT
+  typeset -g _dan_transient_active=1
+  PROMPT=$'\n%F{blue}󰍟%f '
+  RPROMPT='%B%F{blue}%D{%H:%M}%f%b '
+  zle .reset-prompt
+}
+
+_dan_transient_restore() {
+  local previous_status=$?
+  if [[ ${_dan_transient_active:-0} == 1 ]]; then
+    PROMPT=$_dan_full_prompt
+    RPROMPT=$_dan_full_rprompt
+    unset _dan_transient_active _dan_full_prompt _dan_full_rprompt
+  fi
+  return $previous_status
+}
+
+add-zle-hook-widget line-finish _dan_transient_line_finish
+# Run before Starship's precmd hook and preserve the previous command's status.
+precmd_functions=(_dan_transient_restore ${precmd_functions:#_dan_transient_restore})
